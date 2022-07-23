20121453d95f37d4ca32fe6724e2222c424b77a0
chrisyuen976 <chrisyuen976@gmail.com>
2022-07-23 10:57:30 +1000

    Created different tabs, navigate using next and prev. Submittable

--- src/app/components/build-car-form/build-car-form.component.ts
+++ src/app/components/build-car-form/build-car-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
 import { FormService } from 'src/app/services/form-service.service';
 import { FormData } from 'src/models/FormData';
 
@@ -9,7 +9,7 @@ const myChoices = document.getElementById('displayInput') as HTMLInputElement | 
   templateUrl: './build-car-form.component.html',
   styleUrls: ['./build-car-form.component.css']
 })
-export class BuildCarFormComponent implements OnInit {
+export class BuildCarFormComponent implements OnInit, AfterViewInit {
 
   formData: FormData
 
@@ -27,12 +27,27 @@ export class BuildCarFormComponent implements OnInit {
 
   selectedInterior: string;
 
-  constructor(private service : FormService) { }
+  currentTab = 0;
+
+  @ViewChild('formPrevBtn') formPrevBtn: ElementRef;
+  @ViewChild('formNextBtn') formNextBtn: ElementRef;
+  @ViewChildren('tab') tabs: QueryList<any>;
+
+  constructor(private service : FormService) {}
 
   ngOnInit(): void {
     this.service.getFormData().subscribe( (data) => 
     { console.log(data)
       this.formData = data});
+
+    // this.showTab(this.currentTab)
+  }
+
+  ngAfterViewInit(): void {
+    console.log(this.formPrevBtn)
+    console.log(this.tabs)
+
+    this.showTab(this.currentTab);
   }
 
   selectBody($event: any){
@@ -99,6 +114,45 @@ export class BuildCarFormComponent implements OnInit {
     console.log(this.selectedEngine)
     console.log(this.selectedExterior)
     console.log(this.selectedInterior)
+  }
+
+
+
+
+  showTab(n: number){
+    this.tabs.get(n).nativeElement.style.display="block";
+
+    //change buttons depending on page
+    if (n == 0){
+      this.formPrevBtn.nativeElement.style.display="none";
+    } else {
+      this.formPrevBtn.nativeElement.style.display="inline-block";
+    }
+
+    if (n == (this.tabs.length-1)){
+      this.formNextBtn.nativeElement.style.display="none";
+
+    } else {
+      this.formNextBtn.nativeElement.style.display="inline-block";
+    }
+
+    //change step indicator
+  }
+
+  hideTab(n: number){
+    this.tabs.get(n).nativeElement.style.display="none";
+  }
+
+  nextOrPrev(n: number){
+
+    //HIDE
+    this.hideTab(this.currentTab);
+
+    this.currentTab += n;
+    
+    //SHOW
+    this.showTab(this.currentTab);
+    console.log(this.currentTab)
   }
 
 }
